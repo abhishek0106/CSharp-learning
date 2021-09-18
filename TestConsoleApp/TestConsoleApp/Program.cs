0835029cc0fb0f5490ea0535e4c3714be0b8b7bc
Abhishek Mishra <abhishekmishra0106@gmail.com>
2021-09-18 13:27:23 +0530

    Another demo file

---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hii this is just a test. Have a good day !!");
+            Console.Read();
+        }
+    }
+}
